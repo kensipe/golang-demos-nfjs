b23d8ab40ef90a31a5cd06b51df3c508f080a24e
Ken Sipe <kensipe@gmail.com>
2014-09-17 08:11:03 -0500

    adding more examples for strangeloop 2014

---
+++ src/fib.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func fibonacci() func() int {
+    a, b := 0, 1
+	return func() int {
+		a, b = b, a+b
+		return a
+	}
+}
+
+func main() {
+    f := fibonacci()
+    for i := 0; i < 10; i++ {
+        fmt.Println(f())
+    }
+}
+
---
+++ src/stdin.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"os"
+//	"io/ioutil"
+	"bufio"
+	"strconv"
+	"strings"
+)
+
+func main() {
+
+	fmt.Println("Enter Age:")
+
+	reader := bufio.NewReader(os.Stdin)
+	age, _ := reader.ReadString('\n')
+	age = strings.TrimSpace(age)
+	dogyears, _ := strconv.Atoi(age)
+
+	fmt.Print("the age: ", age, " is ", dogyears * 7, " in dog years"  )
+
+}
